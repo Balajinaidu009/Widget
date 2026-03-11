527e42ea6139e69dc33a5e264d22e5e6cef2384e
BalajiNaidu <balaji.suvvada@skyronsoftware.com>
2026-03-11 23:48:11 +0530

    Updated 4 files

--- scripts/Main.js
+++ scripts/Main.js
@@ -5,33 +5,54 @@ function executeWidgetCode() {
                 var contentDiv = document.getElementById("content-display");
                 var dropZoneUI = document.getElementById("drop-zone-ui");
                 
-                // Switch visibility
                 dropZoneUI.style.display = "none";
                 contentDiv.style.display = "block";
                 
                 if(obj.data.items[0].objectType !== "VPMReference"){
-                    contentDiv.innerHTML = "<h4>Not a VPMReference Product</h4><button onclick='location.reload()'>Back</button>";
+                    contentDiv.innerHTML = `
+                        <div class="data-card error-state">
+                            <h4>Invalid Selection</h4>
+                            <p>Please drop a VPMReference Product.</p>
+                            <button class="btn-text" onclick="location.reload()">Back</button>
+                        </div>`;
                 } else {
-                    var tableHTML = "<button id='callApiBtn' class='btn btn-primary'>Send To Vertex</button><div id='apiResult'></div><br/>";
-                    tableHTML += "<table><thead><tr><th>Type</th><th>Name</th><th>ID</th></tr></thead>";
-                    tableHTML += "<tbody><tr><td>"+obj.data.items[0].objectType+"</td><td>"+obj.data.items[0].displayName+"</td><td>"+obj.data.items[0].objectId+"</td></tr></tbody></table>";
-                    contentDiv.innerHTML = tableHTML;
+                    // Modern Card UI instead of Table
+                    var cardHTML = `
+                        <div class="data-card">
+                            <div class="card-header">
+                                <h3>Object Details</h3>
+                                <button class="btn-text" onclick="location.reload()">Reset</button>
+                            </div>
+                            <div class="card-body">
+                                <div class="prop-row"><span>Type</span><strong>${obj.data.items[0].objectType}</strong></div>
+                                <div class="prop-row"><span>Name</span><strong>${obj.data.items[0].displayName}</strong></div>
+                                <div class="prop-row"><span>ID</span><code class="id-badge">${obj.data.items[0].objectId}</code></div>
+                            </div>
+                            <div class="card-footer">
+                                <button id="callApiBtn" class="btn-primary">Send To Vertex</button>
+                            </div>
+                            <div id="apiResult"></div>
+                        </div>`;
+                    contentDiv.innerHTML = cardHTML;
                 }
 
-                document.getElementById("callApiBtn").onclick = function () {
-                    if (confirm("Send " + obj.data.items[0].displayName + " to Vertex?")) {
-                        var url = "https://www.plmtrainer.com:444/Vertex-0.0.1-SNAPSHOT/vertexvis/v1/exportdata?id=" + obj.data.items[0].objectId;
-                        fetch(url, { method: "GET" })
-                        .then(res => res.json())
-                        .then(data => {
-                            const formattedSummary = data["Summary Lines"].replace(/\n/g, "<br>");
-                            document.getElementById("apiResult").innerHTML = "<div class='success-box'>" + formattedSummary + "</div>";
-                        })
-                        .catch(err => {
-                            document.getElementById("apiResult").innerHTML = "<p style='color:red;'>Error: " + err.message + "</p>";
-                        });
-                    }
-                };
+                const apiBtn = document.getElementById("callApiBtn");
+                if (apiBtn) {
+                    apiBtn.onclick = function () {
+                        if (confirm("Send " + obj.data.items[0].displayName + " to Vertex?")) {
+                            var url = "https://www.plmtrainer.com:444/Vertex-0.0.1-SNAPSHOT/vertexvis/v1/exportdata?id=" + obj.data.items[0].objectId;
+                            fetch(url, { method: "GET" })
+                            .then(res => res.json())
+                            .then(data => {
+                                const formattedSummary = data["Summary Lines"].replace(/\n/g, "<br>");
+                                document.getElementById("apiResult").innerHTML = "<div class='success-box'>" + formattedSummary + "</div>";
+                            })
+                            .catch(err => {
+                                document.getElementById("apiResult").innerHTML = "<p class='error-text'>Error: " + err.message + "</p>";
+                            });
+                        }
+                    };
+                }
             },
 
             onLoad: function() {
